b8f569f846f20c9f4f2d68379f24e12363e87a22
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-05-28 00:02:24 +0530

    first commite header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    ProductDetailsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'olxc';
 }
+document.addEventListener('DOMContentLoaded', function () {
+  var sellButton = document.getElementById('sell-button');
+  sellButton === null || sellButton === void 0 ? void 0 : sellButton.addEventListener('click', function () {
+      alert('Sell button clicked!');
+      // Add your sell button click logic here
+  });
+});
